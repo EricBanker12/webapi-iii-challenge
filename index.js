f9f5c59ff952f6ed353a8d29a0405d716afb41a8
ericbanker12 <ericbanker12@gmail.com>
2019-10-30 14:43:35 -0500

    create server and invoke listener in index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code away!
+const express = require('express')
+
+const userRouter = require('./users/userRouter')
+
+const server = express()
+const port = 5000
+
+server.use('/api/users/', userRouter)
+
+server.listen(port, () => {
+    console.log(`=== Listening on localhost:${port} ===`)
+})
